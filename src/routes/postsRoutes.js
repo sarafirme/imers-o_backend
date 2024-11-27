c962953373f43c766aa7cb538d705ce042008e3c
Sara <saravanessaguedesof@gmail.com>
2024-11-26 23:58:07 -0300

    deploy do projeto

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import {atualizarNovoPost, listarPosts, postarNovoPost, uploadImagem} from "../controller/postsController.js";
+
+const crosOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200,
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -14,6 +20,7 @@ const upload = multer({dest:"./uploads", storage})
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(crosOptions));
     app.get("/posts", listarPosts );
     app.post("/posts", postarNovoPost);
     app.post("/upload", upload.single("imagem"), uploadImagem);
